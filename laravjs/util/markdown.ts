1583d4b2b2f8736118b17cde746739f4295eea25
Peter Shang <petershang@canva.com>
2024-08-26 17:08:35 +0800

    Markdown emoji

--- laravjs/util/markdown.ts
+++ laravjs/util/markdown.ts
@@ -1,7 +1,41 @@
 import { dynScript } from "./dom";
 
+const EMOJI_MAPPING = {
+    'no': 10060,
+    'yes': 9989,
+}
+
+function emojify(name) {
+    const cp = EMOJI_MAPPING[name]
+    if (cp) {
+        return `&#${cp}`
+    }
+    return name
+}
+
+const EMOJI_EXTENSION = {
+    name: 'emoji',
+    level: 'inline',
+    start(src) { return src.indexOf(':')},
+    tokenizer(src, tokens) {
+        const rule = /^:(\w+):/
+        const match = rule.exec(src)
+        if (match) {
+            return {
+                type: 'emoji',
+                raw: match[0],
+                emoji: match[1],
+            }
+        }
+    },
+    renderer(token) {
+        return emojify(token.emoji)
+    },
+}
+
 function initMarked() {
     marked.use({
+        breaks: true,
         renderer: {
             link(href: string, title, text) {
                 if (href == null || href == '') {
@@ -21,7 +55,8 @@ function initMarked() {
                 const className = groups[0].substring(1).trim()
                 return `<p class="${className}">${html.substring(groups[0].length)}</p>`
             }
-        }
+        },
+        extensions: [EMOJI_EXTENSION],
     })
 }
 
